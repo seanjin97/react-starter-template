85ee8d086c4983e8156e4f5a7196a164962832ca
Lee Sean Jin <sean_jin_lee@tech.gov.sg>
2024-04-28 19:03:59 +0800

    env demo

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -9,9 +9,12 @@ function Index() {
     return (
         <div>
             <h3>Welcome Home!</h3>
+            <p>Your app is running on {import.meta.env.MODE}</p>
+            <p>Your app is running on {import.meta.env.BASE_URL}</p>
             <Button variant="destructive" onClick={() => alert('hi')}>
                 heloo there
             </Button>
+            <div>Your public env var {import.meta.env.VITE_SOME_KEY}</div>
         </div>
     );
 }
